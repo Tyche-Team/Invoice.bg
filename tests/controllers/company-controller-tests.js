8ddbbfadffa11631379716e0813fa91a2e992164
Strateva <MiroslavaStrateva@gmail.com>
2016-12-05 11:18:34 +0200

    Company-controller-tests (some more)

--- tests/controllers/company-controller-tests.js
+++ tests/controllers/company-controller-tests.js
@@ -1,20 +1,53 @@
-/* globals require describe it */
+/* globals require describe it beforeEach afterEach */
 "use strict";
 
 const { expect } = require("chai"),
-    httpMocks = require('node-mocks-http');
+    sinonModule = require("sinon"),
+    httpMocks = require('node-mocks-http'),
+    events = require('events');
 
 const username = "author",
     userId = 1,
     user = {
         username,
         _id: userId
-    };
+    },
+    userNoSettings = {
+        username: "Not valid",
+        _id: 0
+    },
+    companySetting = {
+        _id: 1,
+        user: userId,
+        name: "Test",
+        bulstat: "0123456789"
+    },
+    companySettings = [companySetting];
 
 describe("Test company controller", () => {
-    let controller = require("../../controllers/company-controller")();
+    let sinon;
+
+    let data = {
+        createCompanySettings() {},
+        getCompanysettings() {},
+        updateCompanysettings() {}
+    };
+
+    beforeEach(() => {
+        sinon = sinonModule.sandbox.create();
+        sinon.stub(data, "getCompanysettings", user => {
+                let currentSetting = companySettings.find(cs => cs._id === user);
+                return Promise.resolve(currentSetting);
+            });
+    });
+    afterEach(() => {
+        sinon.restore();
+    });
+
+    let controller = require("../../controllers/company-controller")(data);
 
     describe("checkCompanySettings()", () => {
+
         it("Expect to redirect to login if no user in request", () => {
             let expectedUrl = "/login";
 
@@ -25,6 +58,37 @@ describe("Test company controller", () => {
 
             let actualUrl = response._getRedirectUrl();
             expect(actualUrl).to.equal(expectedUrl);
+        });
+
+        it("Expect to redirect to company/create if no company for user", () => {
+            let expectedUrl = "/company/create";
+
+            let request = httpMocks.createRequest({
+                    user: userNoSettings
+                }),
+            response = httpMocks.createResponse();
+
+            controller.checkCompanySettings(request, response);
+
+            response.on('end', function() {
+                let actualUrl = response._getRedirectUrl();
+                expect(actualUrl).to.equal(expectedUrl);
+            });
+        });
+
+        it("Expect to redirect to /invoice/all if there is company for user", () => {
+            let expectedUrl = "/invoice/all";
+
+            let request = httpMocks.createRequest({
+                    user
+                }),
+            response = httpMocks.createResponse();
+
+            controller.checkCompanySettings(request, response);
+            response.on('end', function() {
+                let actualUrl = response._getRedirectUrl();
+                expect(actualUrl).to.equal(expectedUrl);
+            });
         });
     });
     describe("getBlankCompanySettings()", () => {
@@ -66,6 +130,72 @@ describe("Test company controller", () => {
 
             let actualUrl = response._getRedirectUrl();
             expect(actualUrl).to.equal(expectedUrl);
+        });
+
+        it("Expect to redirect to company/create if no company for user", () => {
+            let expectedUrl = "/company/create";
+
+            let request = httpMocks.createRequest({
+                    user: userNoSettings
+                }),
+            response = httpMocks.createResponse();
+
+            controller.getCompanySettings(request, response);
+
+            response.on('end', function() {
+                let actualUrl = response._getRedirectUrl();
+                expect(actualUrl).to.equal(expectedUrl);
+            });
+        });
+
+        it("Expect to render to company-details if company found for user", () => {
+            let expectedView = "company-details";
+
+            let request = httpMocks.createRequest({
+                    user: user
+                }),
+            response = httpMocks.createResponse();
+
+            controller.getCompanySettings(request, response);
+
+            response.on('end', function() {
+                let actualView = response._getRenderView();
+                expect(actualView).to.equal(expectedView);
+            });
+        });
+
+        it("Expect to make view rending with correct user", done => {
+            let requset = httpMocks.createRequest({
+                    user
+                }),
+                response = httpMocks.createResponse({
+                    eventEmitter: events.EventEmitter
+                });
+
+            controller.getCompanySettings(requset, response);
+
+            response.on('end', function() {
+                let actualUser = response._getRenderData().user;
+                expect(actualUser).to.eql(user);
+                done();
+            });
+        });
+
+        it("Expect to make view rending with correct model", done => {
+            let requset = httpMocks.createRequest({
+                    user
+                }),
+                response = httpMocks.createResponse({
+                    eventEmitter: events.EventEmitter
+                });
+
+            controller.getCompanySettings(requset, response);
+
+            response.on('end', function() {
+                let actualModel = response._getRenderData().model;
+                expect(actualModel).to.eqls(companySetting);
+                done();
+            });
         });
     });
 
